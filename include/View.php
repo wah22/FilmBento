336cca1fb1c0de18573e9150764da8568fddc468
Daniel Feely <danielfeely@gmail.com>
2011-03-10 20:17:14 +0000

    updated View class to automatically set $data to empty array if not passed to the constructor

--- include/View.php
+++ include/View.php
@@ -2,7 +2,7 @@
 
 class View {
 
-    function load ($filename, $data) {
+    function load ($filename, $data = array()) {
         require_once(ROOT_PATH . '/include/views/' . $filename . '.php');
     }
 }
