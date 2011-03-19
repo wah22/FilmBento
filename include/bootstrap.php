62a65fcffba5ee01f4027638788148af2d960e0a
Daniel <danielfeely@gmail.com>
2011-03-19 11:53:56 +0000

    made some changes to the intiialisation of the app

--- include/bootstrap.php
+++ include/bootstrap.php
@@ -1,10 +1,14 @@
 <?php
 
-ini_set('display_errors',1);
-error_reporting(E_ALL|E_STRICT);
-
 require_once('../config.php');
+
+define('ROOT_PATH', $root_path);
 
 function __autoload($class_name) {
     require_once(ROOT_PATH . '/include/' . $class_name . '.php');
+}
+
+if ($testing == true) {
+    ini_set('display_errors',1);
+    error_reporting(E_ALL|E_STRICT);
 }
