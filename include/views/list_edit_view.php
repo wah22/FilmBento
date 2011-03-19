1b7082428339e42b21f01423b268f67962ccb840
Daniel <danielfeely@gmail.com>
2011-03-19 12:53:12 +0000

    Started working on list drop-down editing

--- include/views/list_edit_view.php
+++ include/views/list_edit_view.php
@@ -1,83 +1,63 @@
-<!DOCTYPE html>
- <html lang="en">
- <head>
-   <meta charset="utf-8">
-   <title></title>
-   <link rel="stylesheet" type="text/css" href="/css/main.css">
-   <link rel="stylesheet" type ="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/humanity/jquery-ui.css">
- </head>
- <body>
-     <?php include ("header.php"); ?>
+<h1><?php echo $data['list']->getName(); ?></h1><br>
+<input type ="hidden" name ="listID" value="<?php echo $data['list']->getID(); ?>">
 
-    <h1><?php echo $data['list']->getName(); ?></h1><br>
-    <input type ="hidden" name ="listID" value="<?php echo $data['list']->getID(); ?>">
+<div id="editListNumbers">
+    <ol>
+        <?php for ($i = 0; $i < 10; $i++) : ?>
+        <li><?php echo $i + 1; ?></li>
+        <?php endfor; ?>
+    </ol>
+</div>
 
-    <div id="editListNumbers">
-        <ol>
-            <?php for ($i = 0; $i < 10; $i++) : ?>
-            <li><?php echo $i + 1; ?></li>
-            <?php endfor; ?>
-        </ol>
-    </div>
+<div id="editList">
 
-    <div id="editList">
+    <ol>
+        <?php $seens = $data['list']->getSeens(); ?>
+        <?php for ($i = 0; $i < 10; $i++) : ?>
+            <?php if (isset($seens[$i])) : ?>
+                <li id="recordsArray_<?php echo $seens[$i]->getFilm()->getID() ?>">
+                    <?php echo $seens[$i]->getFilm()->getTitle(); ?>
+                    <form method="GET" action="">
+                        <input type="hidden" name="controller" value="ListController">
+                        <input type="hidden" name="function" value="removeFromList">
+                        <input type="hidden" name="film" value="<?php echo $seens[$i]->getFilm()->getID(); ?>">
+                        <input type="hidden" name="list" value="<?php echo $data['list']->getID(); ?>">
+                        <input type="submit" value="remove">
+                    </form>
+                 </li>
+            <?php endif; ?>
+         <?php endfor; ?>
+    </ol>
 
-        <ol>
-            <?php $seens = $data['list']->getSeens(); ?>
-            <?php for ($i = 0; $i < 10; $i++) : ?>
-                <?php if (isset($seens[$i])) : ?>
-                    <li id="recordsArray_<?php echo $seens[$i]->getFilm()->getID() ?>">
-                        <?php echo $seens[$i]->getFilm()->getTitle(); ?>
-                        <form method="GET" action="">
-                            <input type="hidden" name="controller" value="ListController">
-                            <input type="hidden" name="function" value="removeFromList">
-                            <input type="hidden" name="film" value="<?php echo $seens[$i]->getFilm()->getID(); ?>">
-                            <input type="hidden" name="list" value="<?php echo $data['list']->getID(); ?>">
-                            <input type="submit" value="remove">
-                        </form>
-                     </li>
-                <?php endif; ?>
-             <?php endfor; ?>
-        </ol>
+    <?php if (count($seens) < 10) : ?>
+        <div id="addFilm">
+            <form method="GET" action="">
+                <input type="hidden" name="controller" value="ListController">
+                <input type="hidden" name="function" value="addToList">
+                <input type="hidden" name="list" value="<?php echo $data['list']->getID(); ?>">
+                <input type="text" name="film" id="tags">
+                <input type="submit" value="Add">
+            </form>
+        </div>
+    <?php endif; ?>
 
-        <?php if (count($seens) < 10) : ?>
-            <div id="addFilm">
-                <form method="GET" action="">
-                    <input type="hidden" name="controller" value="ListController">
-                    <input type="hidden" name="function" value="addToList">
-                    <input type="hidden" name="list" value="<?php echo $data['list']->getID(); ?>">
-                    <input type="text" name="film" id="tags">
-                    <input type="submit" value="Add">
-                </form>
-            </div>
-        <?php endif; ?>
-
-    </div>
-    <?php include "footer.php"; ?>
-</body>
- </html>
+</div>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/jquery-ui.min.js"></script>
 <script>
-    $(function() {
-            var availableTags = <?php echo json_encode($data['films']); ?>;
+$(function() {
+        var availableTags = <?php echo json_encode($data['films']); ?>;
             $( "#tags" ).autocomplete({
                     source: availableTags
             });
-    });
-</script>
-<script>
-$(document).ready(function(){
-	$(function() {
-		$("#editList ol").sortable({ opacity: 0.6, cursor: 'move', update: function() {
-			var order = $(this).sortable("serialize");
-			$.post("/?controller=ListController&list=<?php echo $data['list']->getID(); ?>&function=sort", order, function(theResponse){
-				// $("#contentRight").html(theResponse);
-			});
-		}
-		});
-	});
 
+        $("#editList ol").sortable({ opacity: 0.6, cursor: 'move', update: function() {
+                var order = $(this).sortable("serialize");
+                $.post("/?controller=ListController&list=<?php echo $data['list']->getID(); ?>&function=sort", order, function(theResponse){
+                        // $("#contentRight").html(theResponse);
+                });
+        }
+        });
 });
 </script>
--- include/views/list_index_view.php
+++ include/views/list_index_view.php
@@ -4,18 +4,23 @@
    <meta charset="utf-8">
    <title></title>
    <link rel="stylesheet" type="text/css" href="/css/main.css">
-   <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/themes/smoothness/jquery-ui.css">
+   <link rel="stylesheet" type ="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/humanity/jquery-ui.css">
  </head>
  <body>
-     <?php include ("header.php"); ?>
+    <?php include ("header.php"); ?>
 
-    <?php foreach ($data['user']->getLists() as $list) :  ?>
-
-    <a href="/?controller=ListController&function=edit&list=<?php echo $list->getID(); ?>"><?php echo $list->getName(); ?></a><br>
-
-    <?php endforeach; ?>
+     <div id="top10"></div>
+     <div id="ever"></div>
 
     <?php include "footer.php"; ?>
  </body>
  </html>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/jquery-ui.min.js"></script>
+<script>
+$(function() {
+        $('#top10').load('http://localhost:4492/?controller=ListController&function=edit&list=1');
+        $('#ever').load('http://localhost:4492/?controller=ListController&function=edit&list=2');
+});
+</script>
