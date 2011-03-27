cb18eebedb60ed9a943595abcf6fb74f849da9a2
Daniel <danielfeely@gmail.com>
2011-03-27 17:06:10 +0100

    Started styling of add film page

--- include/views/add_film_view.php
+++ include/views/add_film_view.php
@@ -9,16 +9,41 @@
 <body>
      <?php require_once 'header.php'; ?>
 
-     <h1>add a film</h1>
+    <div id="addFilm">
+        <h1>Add a Film</h1>
 
-     <form id="addFilm" method="POST" action="">
-         <input type="hidden" name="function" value="addFilm">
-         <label for="title">Title</label>
-         <input id="title" name="title" type="text">
-         <label for="year">Year</label>
-         <input id="year" name="year" type="text">
-         <input type="submit" value="Add">
-     </form>
-     <?php include "footer.php"; ?>
+        <form method="POST" action="">
+            <input type="hidden" name="function" value="addFilm">
+
+            <fieldset>
+                <h2>Required Info</h2>
+                <p>
+                    <label for="title">Title</label>
+                    <input id="title" name="title" type="text">
+                </p>
+                <p>
+                    <label for="year">Year</label>
+                    <input id="year" name="year" type="text">
+                </p>
+                <p>
+            </fieldset>
+
+            <fieldset id="links">
+                <h2>Optional Info</h2>
+                <p>
+                    <label for="wikiLink">Wikipedia Link</label>
+                    <input type="text" name="wikiLink">
+                </p>
+                <p>
+                    <label for="rtLink">Rotten Tomatoes Link</label>
+                    <input type="text" name="rtLink">
+                </p>
+            </fieldset>
+                <input type="submit" value="Add">
+            </p>
+        </form>
+    </div>
+
+    <?php include "footer.php"; ?>
  </body>
  </html>
