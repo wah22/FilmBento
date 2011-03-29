def548d4ead283c88b0c31de6874105bc46ce9be
Daniel <danielfeely@gmail.com>
2011-03-29 19:50:53 +0100

    Removed confog from gitignore

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$root_path = "/home/daniel/FilmBento";
+
+$db_host = 'localhost';
+$db_port = '4492';
+$db_name = 'fr';
+$db_username = 'root';
+$db_password = 'password';
+
+$testing = true;
+
