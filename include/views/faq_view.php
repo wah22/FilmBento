0bb367fe4a502098bb1db544e8011bb4fd7f1ecc
Daniel <danielfeely@gmail.com>
2011-03-31 19:36:45 +0100

    Added to the FAQ

--- include/views/faq_view.php
+++ include/views/faq_view.php
@@ -9,8 +9,21 @@
  <body>
     <?php include "header.php" ?>
 
-     <h2>I have found a bug in FilmBento. What should I do? :s</h2>
-     <p>"Drop" me an <a href="mailto:filmbento@gmail.com">email</a>, or report an issue at the <a href ="https://github.com/kitano/FilmBento">Github</a> repo. Thanks</p>
+     <div id="FAQ">
+
+         <article>
+            <h2>I have found a bug in FilmBento. What should I do?</h2>
+            <p>'Drop' me an <a href="mailto:filmbento@gmail.com">email</a>, or report an issue at the <a href ="https://github.com/kitano/FilmBento">github</a> repo. Thanks</p>
+         </article>
+
+         <article>
+        <h2>What is that sexy font?</h2>
+            <p>It's the <a href="http://www.ubuntu.com/">Ubuntu</a> font.</p>
+            <p>You can get more info <a href="http://font.ubuntu.com/">here</a> and it's on <a href="http://www.google.com/webfonts/family?family=Ubuntu&subset=latin">Google web fonts.</a>
+        </article>
+     </div>
+
+
 
 
     <?php include "footer.php"; ?>
--- include/views/film_index_view.php
+++
@@ -1,5 +0,0 @@
-<?php foreach ($data as $film) : ?>
-
-<a href = '<?php echo $film->getPath(); ?>'><?php echo $film->getTitle(); ?></a><br>
-
-<?php endforeach; ?>
