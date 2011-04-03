0b390e98958d3c2c41f4b86a1c234aaa997f508f
Daniel <danielfeely@gmail.com>
2011-04-03 19:44:22 +0100

    Tweaked the footer

--- include/views/footer.php
+++ include/views/footer.php
@@ -4,6 +4,6 @@
         <li><a href="contact">Contact</a></li>
         <li><a href="about">About</a></li>
     </ul>
-    FilmBento</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.<br>
-    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/80x15.png" /></a
+    Copyright &copy; 2011 FilmBento. Licensed under the <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>. <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><!--<img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/80x15.png"></a>-->
+   
 </footer>
