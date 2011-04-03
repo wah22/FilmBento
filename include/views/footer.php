5f3ba19b7f0df77a77deca5202ad6ced0d02457f
Daniel <danielfeely@gmail.com>
2011-04-03 14:02:49 +0100

    Added the license

--- include/views/footer.php
+++ include/views/footer.php
@@ -1,8 +1,9 @@
 <footer>
     <ul>
-        <li>Copyright &copy; 2011 FilmBento</li>
         <li><a href="/?controller=FAQController">FAQ</a></li>
         <li><a href="contact">Contact</a></li>
         <li><a href="about">About</a></li>
     </ul>
+    FilmBento</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.<br>
+    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/80x15.png" /></a
 </footer>
