cd66a966d5f0bca62b05fede67926c6079a4dca9
Daniel <danielfeely@gmail.com>
2011-04-09 14:22:20 +0100

    Re-arranged the film page

--- include/views/film_view.php
+++ include/views/film_view.php
@@ -13,109 +13,114 @@
         <?php include "header.php" ?>
 
         <div id="film">
-            <div id="details">
-                <p>Added by <a href="<?php echo $data['addedBy']->getPath(); ?>"><?php echo $data['addedBy']->getHandle(); ?></a></p>
-                <?php if($data['user']) : ?>
-                <a href="/?controller=FilmController&film=<?php echo urlencode($data['film']->getTitle()); ?>&function=edit">edit/add details</a>
-                <?php endif; ?>
-            </div>
-
-            <?php if ($data['film']->getMeta('poster_link')) : ?>
-            <img src="<?php echo $data['film']->getMeta('poster_link'); ?>" style="height: 270px; position: absolute;">
-            <?php endif; ?>
-
-            <h1><?php echo $data['film']->getTitle(); ?></h1>
-            <h2>(<?php echo $data['film']->getyear(); ?>)</h2>
-
-            <div id="links">
-                <ul>
-                <?php if ($data['film']->getMeta('wiki_link')) : ?>
-                    <li>
-                        <a href="<?php echo $data['film']->getMeta('wiki_link'); ?>">Wikipedia</a>
-                    </li>
+            <div id="top">
+                <?php if ($data['film']->getMeta('poster_link')) : ?>
+                <img id="poster" src="<?php echo $data['film']->getMeta('poster_link'); ?>">
                 <?php endif; ?>
 
-                <?php if ($data['film']->getMeta('rt_link')) : ?>
-                    <li>
-                        <a href="<?php echo $data['film']->getMeta('rt_link'); ?>">Rotten Tomatoes</a>
-                    </li>
-                <?php endif; ?>
-
-                <?php if ($data['film']->getMeta('imdb_link')) : ?>
-                    <li>
-                        <a href="<?php echo $data['film']->getMeta('imdb_link'); ?>">IMDb</a>
-                    </li>
-                <?php endif; ?>
-
-                <?php if ($data['film']->getMeta('metacritic_link')) : ?>
-                    <li>
-                        <a href="<?php echo $data['film']->getMeta('metacritic_link'); ?>">Metacritic</a>
-                    </li>
-                <?php endif; ?>
-                </ul>
-            </div>
-
-            <?php if($data['user']) : ?>
-
-            <?php if (!$data['hasSeen']) : ?>
-            <form method="post" action="">
-                <input type="hidden" name="function" value="seen">
-                <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
-                <input type ="submit" value="Seen it" id="seenIt">
-           </form>
-           <?php endif; ?>
-
-            <?php if ($data['hasSeen']) : ?>
-            <div id="rate">
-                <fieldset id ="seen">
-                    <legend>Seen</legend>
-                    <?php echo $data['whenSeen']; ?>
-                    <form method="post" action="" id="unSee">
-                        <input type="hidden" name="function" value="unsee">
-                        <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
-                        <input type ="image" src="/images/icons/x.png">
-                   </form>
-                </fieldset>
-                <fieldset>
-                    <legend>My rating</legend>
-                    <?php for( $i = 1; $i <= 5 ; $i++ ) : ?>
-                    <form method="post" action="">
-                        <input type="hidden" name="function" value="rate">
-                        <input type="hidden" name="rating" value="<?php echo $i; ?>">
-                        <?php if ($data['hasRated'] && $i <= $data['rating']) : ?>
-                        <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_filled.png" style="float:left">
-                        <?php else : ?>
-                        <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_empty.png" style="float:left">
-                        <?php endif; ?>
-                    </form>
-                    <?php endfor; ?>
-                    <br>
-
-                    <?php if (empty($data['tweeview'])) : ?>
-                    <a href="#" id="showTweeview">
-                        <img src="/images/icons/comment_add.png">write tweeview
-                    </a>
-                    <div id="tweeview">
-                        <form method="post" action="">
-                            <input type="hidden" name="function" value="tweeview">
-                            <textarea name="tweeview" rows="7" maxlength="140"></textarea>
-                            <input type="submit" name="submit" value="Submit">
-                        </form>
-                    </div>
-                    <?php else : ?>
-                    <div id="tweeviewShown"><?php echo $data['tweeview']; ?></div>
+                <div id="details">
+                    <p>Added by <a href="<?php echo $data['addedBy']->getPath(); ?>"><?php echo $data['addedBy']->getHandle(); ?></a></p>
+                    <?php if($data['user']) : ?>
+                    <a href="/?controller=FilmController&film=<?php echo urlencode($data['film']->getTitle()); ?>&function=edit">edit/add details</a>
                     <?php endif; ?>
-                </fieldset>
+                </div>
+
+                <h1><?php echo $data['film']->getTitle(); ?></h1>
+                <h2>(<?php echo $data['film']->getyear(); ?>)</h2>
+
+                <div id="links">
+                    <ul>
+                    <?php if ($data['film']->getMeta('wiki_link')) : ?>
+                        <li>
+                            <a href="<?php echo $data['film']->getMeta('wiki_link'); ?>">Wikipedia</a>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if ($data['film']->getMeta('rt_link')) : ?>
+                        <li>
+                            <a href="<?php echo $data['film']->getMeta('rt_link'); ?>">Rotten Tomatoes</a>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if ($data['film']->getMeta('imdb_link')) : ?>
+                        <li>
+                            <a href="<?php echo $data['film']->getMeta('imdb_link'); ?>">IMDb</a>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if ($data['film']->getMeta('metacritic_link')) : ?>
+                        <li>
+                            <a href="<?php echo $data['film']->getMeta('metacritic_link'); ?>">Metacritic</a>
+                        </li>
+                    <?php endif; ?>
+                    </ul>
+                </div>
+
+                <?php if($data['user']) : ?>
+
+                <?php if (!$data['hasSeen']) : ?>
+                <form method="post" action="">
+                    <input type="hidden" name="function" value="seen">
+                    <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
+                    <input type ="submit" value="Seen it" id="seenIt">
+               </form>
+               <?php endif; ?>
+
+                <?php if ($data['hasSeen']) : ?>
+                <div id="userPanel">
+                    <fieldset id ="seen">
+                        <legend>Seen</legend>
+                        <?php echo $data['whenSeen']; ?>
+                        <form method="post" action="" id="unSee">
+                            <input type="hidden" name="function" value="unsee">
+                            <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
+                            <input type ="image" src="/images/icons/x.png">
+                       </form>
+                    </fieldset>
+                    <fieldset id="rate">
+                        <legend>My rating</legend>
+                        <div id="rating">
+                            <?php for( $i = 1; $i <= 5 ; $i++ ) : ?>
+                            <form method="post" action="">
+                                <input type="hidden" name="function" value="rate">
+                                <input type="hidden" name="rating" value="<?php echo $i; ?>">
+                                <?php if ($data['hasRated'] && $i <= $data['rating']) : ?>
+                                <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_filled.png" style="float:left">
+                                <?php else : ?>
+                                <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_empty.png" style="float:left">
+                                <?php endif; ?>
+                            </form>
+                            <?php endfor; ?>
+                        </div>
+
+                        <?php if (empty($data['tweeview'])) : ?>
+                        <p>
+                            <a href="#" id="showTweeview">
+                                <img src="/images/icons/comment_add.png">write tweeview
+                            </a>
+                        </p>
+                        <div id="tweeview">
+                            <form method="post" action="">
+                                <input type="hidden" name="function" value="tweeview">
+                                <textarea name="tweeview" rows="3" maxlength="140"></textarea>
+                                <input type="submit" name="submit" value="Submit">
+                            </form>
+                        </div>
+                        <?php else : ?>
+                        <div id="tweeviewShown"><?php echo $data['tweeview']; ?></div>
+                        <?php endif; ?>
+                    </fieldset>
+                </div>
+                <?php endif; ?>
+                <?php endif; ?>
             </div>
-            <?php endif; ?>
-            <?php endif; ?>
 
             <div class="list" id="recentlySeen">
                 <h1>Recently seen by</h1>
                 <ul>
                     <?php foreach($data['recentlySeens'] as $seen) : ?>
                     <li>
-                        <h2><a href="<?php echo $seen['path']; ?>"><?php echo $seen['user']; ?></a></h2>
+                        <h3><a href="<?php echo $seen['path']; ?>"><?php echo $seen['user']; ?></a></h3>
                         <div id="stars">
                             <?php for ($i = 0; $i < $seen['rating']; $i++) : ?>
                             <img src="/images/stars/star_filled.png">
