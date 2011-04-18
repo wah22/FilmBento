f10d5beecd872a9ac5d7560dc9b2b4016e1218ac
Daniel <danielfeely@gmail.com>
2011-04-18 17:39:04 +0100

    Rearranged the add film page

--- include/views/add_film_view.php
+++ include/views/add_film_view.php
@@ -15,50 +15,48 @@
 
             <form method="POST" action="">
                 <fieldset>
-                    <fieldset>
-                        <legend>Details</legend>
-                        <input type="hidden" name="function" value="addFilm">
-                        
-                        <label for="title">Title</label>
-                        <input id="title" name="title" type="text">
+                    <legend>Details</legend>
+                    <input type="hidden" name="function" value="addFilm">
+                    
+                    <label for="title">Title</label>
+                    <input id="title" name="title" type="text">
 
-                        <label for="year">Year</label>
-                        <input id="year" name="year" type="text">
+                    <label for="year">Year</label>
+                    <input id="year" name="year" type="text">
 
-                        <label for="poster_url">Poster URL</label>
-                        <input id="poster_url" name="poster_url" type="text">
+                    <label for="poster_url">Poster URL</label>
+                    <input id="poster_url" name="poster_url" type="text">
 
-                        <label for="hashtag">Hashtag</label>
-                        <input id="hashtag" name="hashtag" type="text">
-                    </fieldset>
-
-                    <fieldset>
-                        <legend>External URLs</legend>
-                        <label for="wiki_link">Wikipedia</label>
-                        <input id="wiki_link" name="wiki_link" type="text">
-
-                        <label for="rt_link">Rotten Tomatoes</label>
-                        <input id="rt_link" name="rt_link" type="text">
-
-                        <label for="imdb_link">IMDb</label>
-                        <input id="imdb_link" name="imdb_link" type="text">
-
-                        <label for="metacritic_link">Metacritic</label>
-                        <input id="metacritic_link" name="metacritic_link" type="text">
-                    </fieldset>
-
-                    <div class="notes">
-                        <p>Please make sure the info you enter is correct.</p>
-                    </div>
-                    <?php if (!empty($data['errors'])) : ?>
-                    <div id="errors">
-                        <?php foreach($data['errors'] as $error) : ?>
-                        <p><?php echo $error; ?></p>
-                        <?php endforeach; ?>
-                    </div>
-                    <?php endif; ?>
-                    <button name="submit" class="big button"><span class="check icon"></span>Add</button>
+                    <label for="hashtag">Hashtag</label>
+                    <input id="hashtag" name="hashtag" type="text">
                 </fieldset>
+
+                <fieldset>
+                    <legend>External URLs</legend>
+                    <label for="wiki_link">Wikipedia</label>
+                    <input id="wiki_link" name="wiki_link" type="text">
+
+                    <label for="rt_link">Rotten Tomatoes</label>
+                    <input id="rt_link" name="rt_link" type="text">
+
+                    <label for="imdb_link">IMDb</label>
+                    <input id="imdb_link" name="imdb_link" type="text">
+
+                    <label for="metacritic_link">Metacritic</label>
+                    <input id="metacritic_link" name="metacritic_link" type="text">
+                </fieldset>
+
+                <div class="notes">
+                    <p>Please make sure the info you enter is correct.</p>
+                </div>
+                <?php if (!empty($data['errors'])) : ?>
+                <div id="errors">
+                    <?php foreach($data['errors'] as $error) : ?>
+                    <p><?php echo $error; ?></p>
+                    <?php endforeach; ?>
+                </div>
+                <?php endif; ?>
+                <button name="submit" class="big button"><span class="check icon"></span>Add</button>
             </form>
         </div>
 
