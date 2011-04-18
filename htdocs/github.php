bdd723ad42d2fc88397f9c7f67c3c68800ec42f5
Daniel <danielfeely@gmail.com>
2011-04-18 17:56:52 +0100

    testing github.php

--- htdocs/github.php
+++ htdocs/github.php
@@ -1,1 +1,8 @@
-<?php `git pull`
+<?php 
+
+$command = 'git pull';
+// Execute the shell command
+$shellOutput = shell_exec($command.' > /dev/null; echo $?');
+   
+//return execute status;
+echo trim($shellOutput); 
