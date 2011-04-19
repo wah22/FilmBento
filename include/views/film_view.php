3228c38db2f77cbc933d5910f25ea3509768381d
Daniel <danielfeely@gmail.com>
2011-04-19 21:42:03 +0100

    Majorly improved the film page, after a lot of fiddling

--- include/views/film_view.php
+++ include/views/film_view.php
@@ -21,29 +21,29 @@
                     <li>
                         <div id="links">
                             <ul>
-                            <?php if ($data['film']->getMeta('wiki_link')) : ?>
+                                <?php if ($data['film']->getMeta('wiki_link')) : ?>
                                 <li>
                                     <a href="<?php echo $data['film']->getMeta('wiki_link'); ?>">Wikipedia</a>
                                 </li>
-                            <?php endif; ?>
+                                <?php endif; ?>
 
-                            <?php if ($data['film']->getMeta('rt_link')) : ?>
+                                <?php if ($data['film']->getMeta('rt_link')) : ?>
                                 <li>
                                     <a href="<?php echo $data['film']->getMeta('rt_link'); ?>">Rotten Tomatoes</a>
                                 </li>
-                            <?php endif; ?>
+                                <?php endif; ?>
 
-                            <?php if ($data['film']->getMeta('imdb_link')) : ?>
+                                <?php if ($data['film']->getMeta('imdb_link')) : ?>
                                 <li>
                                     <a href="<?php echo $data['film']->getMeta('imdb_link'); ?>">IMDb</a>
                                 </li>
-                            <?php endif; ?>
+                                <?php endif; ?>
 
-                            <?php if ($data['film']->getMeta('metacritic_link')) : ?>
+                                <?php if ($data['film']->getMeta('metacritic_link')) : ?>
                                 <li>
                                     <a href="<?php echo $data['film']->getMeta('metacritic_link'); ?>">Metacritic</a>
                                 </li>
-                            <?php endif; ?>
+                                <?php endif; ?>
                             </ul>
                         </div>
                     </li>
@@ -91,72 +91,11 @@
             </div>
 
             <div id="mainColumn">
-                <div id="details">
-                    <p>Added by <a href="<?php echo $data['addedBy']->getPath(); ?>"><?php echo $data['addedBy']->getHandle(); ?></a></p>
-                    <?php if($data['user']) : ?>
-                    <a href="/?controller=FilmController&film=<?php echo urlencode($data['film']->getTitle()); ?>&function=edit">edit/add details</a>
-                    <?php endif; ?>
-                </div>
                 <hgroup id="title">
                     <h1><?php echo $data['film']->getTitle(); ?></h1>
                     <h2>(<?php echo $data['film']->getyear(); ?>)</h2>
                 </hgroup>
-                <div id="userPanel">
-                    <?php if($data['user']) : ?>
-                    <?php if (!$data['hasSeen']) : ?>
-                    <form method="post" action="">
-                        <input type="hidden" name="function" value="seen">
-                        <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
-                        <input type ="submit" value="Seen it" id="seenIt">
-                    </form>
-                    <?php endif; ?>
-                    <?php if ($data['hasSeen']) : ?>
-                        <div id ="seen">
-                            <fieldset>
-                                <legend>Seen</legend>
-                                <?php echo $data['whenSeen']; ?>
-                                <form method="post" action="" id="unSee">
-                                    <input type="hidden" name="function" value="un-see">
-                                    <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
-                                    <button class="negative button" id="unSeeButton"><span class="cross icon"></span>unsee</button>
-                               </form>
-                            </fieldset>
-                        </div>
-                        <fieldset id="rate">
-                            <legend>My rating</legend>
-                            <div id="rating">
-                                <?php for( $i = 1; $i <= 5 ; $i++ ) : ?>
-                                <form method="post" action="" class="rateStar">
-                                    <input type="hidden" name="function" value="rate">
-                                    <input type="hidden" name="rating" value="<?php echo $i; ?>">
-                                    <?php if ($data['hasRated'] && $i <= $data['rating']) : ?>
-                                    <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_filled.png" style="float:left">
-                                    <?php else : ?>
-                                    <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_empty.png" style="float:left">
-                                    <?php endif; ?>
-                                </form>
-                                <?php endfor; ?>
-                            </div>
 
-                            <?php if (empty($data['tweeview'])) : ?>
-                            <br>
-                            <a href="#" id="showTweeview">
-                                write tweeview
-                            </a>
-                            <div id="tweeview">
-                                <form method="post" action="">
-                                    <input type="hidden" name="function" value="tweeview">
-                                    <textarea name="tweeview" rows="3" maxlength="140"></textarea>
-                                    <input type="submit" name="submit" value="Submit">
-                                </form>
-                            </div>
-                            <?php else : ?>
-                            <blockquote id="tweeviewShown">&ldquo;<?php echo $data['tweeview']; ?>&rdquo;</blockquote>
-                            <?php endif; ?>
-                        </fieldset>
-                    <?php endif; ?>
-                    <?php endif; ?>
-                </div>
                 <div class="list" id="recentlySeen">
                     <h1>Recently seen by</h1>
                     <ul>
@@ -176,14 +115,75 @@
                     </ul>
                 </div>
             </div>
+
+            <div id="rightColumn">
+                <div id="details">
+                    <p>Added by <a href="<?php echo $data['addedBy']->getPath(); ?>"><?php echo $data['addedBy']->getHandle(); ?></a></p>
+                    <?php if($data['user']) : ?>
+                    <a href="/?controller=FilmController&film=<?php echo urlencode($data['film']->getTitle()); ?>&function=edit">edit/add details</a>
+                    <?php endif; ?>
+                </div>
+                <?php if($data['user']) : ?>
+                <?php if (!$data['hasSeen']) : ?>
+                <form method="post" action="">
+                    <input type="hidden" name="function" value="seen">
+                    <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
+                    <input type ="submit" value="Seen it" id="seenIt">
+                </form>
+                <?php endif; ?>
+                <?php if ($data['hasSeen']) : ?>
+                    <div id ="seen">
+                        <fieldset>
+                            <legend>Seen</legend>
+                            <?php echo $data['whenSeen']; ?>
+                            <form method="post" action="" id="unSee">
+                                <input type="hidden" name="function" value="un-see">
+                                <input type="hidden" name="film" value="<?php echo $data['film']->getID(); ?>">
+                                <button class="negative button" id="unSeeButton"><span class="cross icon"></span>unsee</button>
+                           </form>
+                        </fieldset>
+                    </div>
+                    <fieldset id="rate">
+                        <legend>My Rating</legend>
+                        <div id="rating">
+                            <?php for( $i = 1; $i <= 5 ; $i++ ) : ?>
+                            <form method="post" action="" class="rateStar">
+                                <input type="hidden" name="function" value="rate">
+                                <input type="hidden" name="rating" value="<?php echo $i; ?>">
+                                <?php if ($data['hasRated'] && $i <= $data['rating']) : ?>
+                                <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_filled.png" style="float:left">
+                                <?php else : ?>
+                                <input type="image" name="rating" value="<?php echo $i; ?>" src="/images/stars/star_empty.png" style="float:left">
+                                <?php endif; ?>
+                            </form>
+                            <?php endfor; ?>
+                        </div>
+                    </fieldset>
+                <?php endif; ?>
+                <?php endif; ?>
+                <?php if ($data['hasRated']) : ?>
+                <div id="tweeview">
+                    <fieldset>
+                        <legend>My Tweeview</legend>
+                        <?php if (empty($data['tweeview'])) : ?>
+                        <form method="post" action="">
+                            <input type="hidden" name="function" value="tweeview">
+                            <textarea name="tweeview" rows="3" maxlength="140"></textarea>
+                            <input type="submit" name="submit" value="Submit">
+                        </form>
+                        <?php else : ?>
+                        <?php echo $data['tweeview']; ?>
+                        <?php endif; ?>
+                    </fieldset>
+                </div>
+                <?php endif; ?>
+            </div>
         </div>
         <?php include "footer.php"; ?>
      </div>
      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js" type="text/javascript"></script>
      <script type="text/javascript">
          $(function() {
-            $('#tweeview').hide();
-
             $('#showTweeview').click(function(){
                 $('#tweeview').slideToggle(230);
                 return false;
@@ -193,8 +193,7 @@
                 var filmID = $(this).parent().find('input[name=film]').val();
 
                 $.post("", { controller: "FilmController", "function": "seen", "film": filmID }, function(page) {
-                    var userPanel = $(page).find('#userPanel');
-                    $('#userPanel').html(userPanel);
+                    $('#rightColumn').load('<?php echo $data['film']->getPath(); ?> #rightColumn');
                 } );
 
                 return false;
@@ -206,8 +205,7 @@
                     var filmID = $(this).parent().find('input[name=film]').val();
 
                     $.post("", { controller: "FilmController", "function": "unSee", "film": filmID }, function(page) {
-                        var userPanel = $(page).find('#userPanel');
-                        $('#userPanel').html(userPanel);
+                        $('#rightColumn').load('<?php echo $data['film']->getPath(); ?> #rightColumn');
                     } );
                     return false;
                 };
@@ -227,8 +225,7 @@
                 }
 
                 $.post("", { controller: "FilmController", "function": "rate", "rating": rating }, function(page) {
-                    var userPanel = $(page).find('#userPanel');
-                    $('#userPanel').html(userPanel);
+                    $('#rightColumn').load('<?php echo $data['film']->getPath(); ?> #rightColumn');
                 });
                 
                 return false;
