338a5485bfd6819428b348c513d011de3f22b954
Daniel <danielfeely@gmail.com>
2011-04-22 16:24:55 +0100

    Now uses the remote database

--- config.php
+++ config.php
@@ -3,11 +3,10 @@
 $root_path = "/var/www/FilmBento";
 $base_url = "http://localhost:4444";
 
-$db_host = 'localhost';
-$db_port = '4444';
+$db_host = '192.168.1.34';
 $db_name = 'fbo';
 $db_username = 'root';
 $db_password = 'password';
 
-$testing = false;
+$testing = true;
 
--- include/DB.php
+++ include/DB.php
@@ -3,17 +3,15 @@
 class DB {
     private static $instance;
     
-    private function __contruct() {
+    private function __construct() {
     }
 
     public static function getInstance() {
         if (empty(self::$instance)) {
-            self::$instance = new PDO('mysql:
-                                      host=' . DB_HOST . ';
-                                      dbname=' . DB_NAME . ';
-                                      port='. DB_PORT,
+            self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
                                       DB_USERNAME,
                                       DB_PASSWORD);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
         return self::$instance;
     }
