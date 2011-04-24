b91cb11614cbcaf1aac7beecd4cdfee71a1587f4
Daniel <danielfeely@gmail.com>
2011-04-24 19:50:52 +0100

    Some work on the front page style

--- include/views/user_home_view.php
+++ include/views/user_home_view.php
@@ -9,60 +9,63 @@
 <body>
     <div id="pageWrapper">
         <?php include ROOT_PATH . '/include/views/header.php'; ?>
-        <div id="whatSeen">
-            <h1>what have you seen?</h1>
-            <form method="POST" action="">
-                <input type ="hidden" name="function" value="seen">
-                <input type="text" name ="film" id="tags">
-                <input type="submit" id="goButton" disabled="true" value="Go">
-            </form>
-            <div id="add">
-                 Can't find? <a href="/?controller=AddFilmController">Add a film.</a>
+        <div id="userHome">
+            <div id="whatSeen">
+                <h1>what have you seen?</h1>
+                <form method="POST" action="">
+                    <input type ="hidden" name="function" value="seen">
+                    <input type="text" name ="film" id="tags">
+                    <input type="submit" id="goButton" disabled="true" value="Go">
+                </form>
+                <div id="add">
+                     Can't find? <a href="/?controller=AddFilmController">Add a film.</a>
+                </div>
             </div>
-        </div>
-        <div id="haveYouSeen">
-            <h2>Recently Added - have you seen?</h2>
-            <ul>
-                <?php foreach ($data['recentlyAddedFilms'] as $film) : ?>
-                <li>
-                    <a href="<?php echo $film['film']->getPath(); ?>">
-                        <?php if ($film['film']->getMeta('poster_link')) : ?>
-                        <img class="poster" src="<?php echo $film['film']->getMeta('poster_link'); ?>">
-                        <?php endif; ?>
-                        <?php echo $film['film']->getTitle(); ?>
-                    </a>
-                    <p>
-                    <?php for($i = 0; $i < $film['averageRating']; $i++ ) : ?>
-                    <img class="star" src="/images/stars/star_filled.png">
-                    <?php endfor; ?>
-                    </p>
-                    <?php if (!empty($film['recentSeens'])) : ?>
-                    <?php foreach ($film['recentSeens'] as $seen) : ?>
-                    <div class="tweeview">
-                        &ldquo;<?php echo $seen['seen']->getTweeview(); ?>&rdquo;
-                        <div class="signature">
-                            <a href="<?php echo $seen['user']->getPath(); ?>">- <?php echo $seen['user']->getHandle(); ?></a>
+            <div id="haveYouSeen">
+                <h2>Recently Added - have you seen?</h2>
+                <ul>
+                    <?php foreach ($data['recentlyAddedFilms'] as $film) : ?>
+                    <li>
+                        <a href="<?php echo $film['film']->getPath(); ?>">
+                            <?php if ($film['film']->getMeta('poster_link')) : ?>
+                            <img class="poster" src="<?php echo $film['film']->getMeta('poster_link'); ?>">
+                            <?php endif; ?>
+                            <div class="title"><?php echo $film['film']->getTitle(); ?></div>
+                        </a>
+                        <p>
+                        <!--<?php //for($i = 0; $i < $film['averageRating']; $i++ ) : ?>
+                        <img class="star" src="/images/stars/star_filled.png">
+                        <?php //endfor; ?>-->
+                        </p>
+                        <?php if (!empty($film['recentSeens'])) : ?>
+                        <?php foreach ($film['recentSeens'] as $seen) : ?>
+                        <div class="tweeview">
+                            &ldquo;<?php echo $seen['seen']->getTweeview(); ?>&rdquo;
+                            <div class="signature">
+                                <a href="<?php echo $seen['user']->getPath(); ?>">- <?php echo $seen['user']->getHandle(); ?></a>
+                            </div>
                         </div>
-                    </div>
+                        <?php endforeach; ?>
+                        <?php endif; ?>
+                    </li>
                     <?php endforeach; ?>
-                    <?php endif; ?>
-                </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-        <div class="list">
-            <h2>Recently Seen</h2>
-            <ul>
-                <?php foreach($data['recentlySeens'] as $seen) : ?>
-                <li>
-                    <a href="<?php echo $seen['user']->getPath(); ?>"><?php echo $seen['user']->getHandle(); ?></a> saw
-                    <a href="<?php echo $seen['film']->getpath(); ?>"><?php echo $seen['film']->getTitle(); ?></a>
-                    <?php if ($seen['seen']->getRating()) : ?>
-                    and rated it <?php echo $seen['seen']->getRating(); ?> stars.
-                    <?php endif; ?>
-                </li>
-                <?php endforeach; ?>
-            </ul>
+                </ul>
+                <div style="clear: both;"></div>
+            </div>
+            <div class="list">
+                <h2>Recently Seen</h2>
+                <ul>
+                    <?php foreach($data['recentlySeens'] as $seen) : ?>
+                    <li>
+                        <a href="<?php echo $seen['user']->getPath(); ?>"><?php echo $seen['user']->getHandle(); ?></a> saw
+                        <a href="<?php echo $seen['film']->getpath(); ?>"><?php echo $seen['film']->getTitle(); ?></a>
+                        <?php if ($seen['seen']->getRating()) : ?>
+                        and rated it <?php echo $seen['seen']->getRating(); ?> stars.
+                        <?php endif; ?>
+                    </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
         </div>
         <?php include ROOT_PATH . "/include/views/footer.php"; ?>
     </div>
