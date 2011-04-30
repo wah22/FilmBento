d4a4a2c11846b7f46806f6b93d1b91f8c80b184f
kitano <danielfeely@gmail.com>
2011-04-30 21:45:53 +0100

    Fixed typing error in page title

--- include/views/user_home_view.php
+++ include/views/user_home_view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Filmbento / Home</title>
+    <title>FilmBento / Home</title>
     <link href='http://fonts.googleapis.com/css?family=Ubuntu:light,lightitalic,regular,italic,500,500italic,bold,bolditalic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="/css/main.css">
     <link rel="stylesheet" type ="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/humanity/jquery-ui.css">
