b64281062af6d92c19176c79cb97176472cd4426
kitano <danielfeely@gmail.com>
2011-05-01 14:52:08 +0100

    Added statcounter code to the footer

--- include/views/footer.php
+++ include/views/footer.php
@@ -7,3 +7,18 @@
     </ul>
     Copyright &copy; 2011 FilmBento. Licensed under <a href="http://www.gnu.org/licenses/gpl.html">GPLv3</a>.
 </footer>
+
+<!-- Start of StatCounter Code -->
+<script type="text/javascript">
+var sc_project=6843054;
+var sc_invisible=1;
+var sc_security="d6b25fcb";
+</script>
+
+<script type="text/javascript"
+src="http://www.statcounter.com/counter/counter.js"></script><noscript><div
+class="statcounter"><a title="hit counter joomla"
+href="http://statcounter.com/joomla/" target="_blank"><img
+class="statcounter" src="http://c.statcounter.com/6843054/0/d6b25fcb/1/"
+alt="hit counter joomla" ></a></div></noscript>
+<!-- End of StatCounter Code -->
