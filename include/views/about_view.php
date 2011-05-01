ac2526208317b40a72e0a8400bbfbb3afdb39f6c
kitano <danielfeely@gmail.com>
2011-05-01 10:52:36 +0100

    Minor changes to about page

--- include/views/about_view.php
+++ include/views/about_view.php
@@ -11,10 +11,10 @@
         <div id="about">
             <h1>About Filmbento</h1>
             <p>FilmBento is entirely open-source. The code can be found <a href="https://github.com/kitano/FilmBento">here</a>.</p>
-            <p>It's a simple website that I have created to solve a need I've had for a while now - to be able to keep track of what I watched what I thought of them, and have the ability to share that.</p>
+            <p>It's a simple website that I have created to solve a need I've had for a while now - to be able to keep track of what films I've watched and what I thought of them, and have the ability to share that.</p>
             <p>It was heavily inspired by <a href="http://www.last.fm/">Last.fm</a> and scrobbling of music and is basically the same idea but based around film watching.</p>
             <p>I started coding the site in about December of 2010, with a new-found knowledge of PHP, MySQL, HTML and Apache.</p>
-            <p>The site runs on a LAMP stack on a <a href="http://mediatemple.net/">Media Temple</a> Ubuntu server. It is hand-coded froms scratch in PHP and HTML. It was built using open-source tools on Ubuntu 10.10 and 11.04.</p>
+            <p>The site runs on a LAMP stack on a <a href="http://mediatemple.net/">Media Temple</a> Ubuntu server. It is hand-coded from scratch in PHP and HTML. It was built using open-source tools on Ubuntu 10.10 and 11.04.</p>
         </div>
         <?php include "footer.php"; ?>
     </div>
