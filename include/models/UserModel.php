b01fd25e1fd3122d3e607ad08c03a400612a365f
kitano <danielfeely@gmail.com>
2011-05-02 15:31:39 +0100

    Cleaned up code, documented, and converted it to use it's own pdo instance instead of the DB singleton

--- include/models/UserModel.php
+++ include/models/UserModel.php
@@ -1,13 +1,19 @@
 <?php
 
-class UserModel {
+class UserModel extends Model {
 
-    function create($email, $handle, $password) {
+    function __construct() {
+        parent::__construct();
+    }
+
+    // Add a user to the database.
+    public function create($email, $handle, $password) {
+        // encrypt the password
         $password = md5($password);
 
+        // If the user doesn't already exist in the database insert it.
         if (! $this->getUser('handle', $handle) && !$this->getUser('email', $email)) {
-            // save user info
-            $stmt = DB::getInstance()->prepare('INSERT INTO fr_users VALUES ( NULL, :email, :handle, :password, 0000-00-00, "", 00, "", "user")');
+            $stmt = $this->pdo->prepare('INSERT INTO fr_users VALUES ( NULL, :email, :handle, :password, 0000-00-00, "", 00, "", "user")');
             $stmt->bindParam(':email', $email);
             $stmt->bindParam(':handle', $handle);
             $stmt->bindParam(':password', $password);
@@ -17,13 +23,16 @@ class UserModel {
         }
     }
 
+    // Gets a user from the database and returns it.
+    // The $by variable is the criteria you want to use to select the user eg, email
     function getUser($by, $value) {
+        // Set the query based on the get criteria
         if ($by == 'handle') {
-            $stmt = DB::getInstance()->prepare('SELECT * FROM fr_users WHERE handle = :value');
+            $stmt = $this->pdo->prepare('SELECT * FROM fr_users WHERE handle = :value');
         } else if ($by == 'id') {
-            $stmt = DB::getInstance()->prepare('SELECT * FROM fr_users WHERE id = :value');
+            $stmt = $this->pdo->prepare('SELECT * FROM fr_users WHERE id = :value');
         } else if ($by == 'email') {
-             $stmt = DB::getInstance()->prepare('SELECT * FROM fr_users WHERE email = :value');
+             $stmt = $this->pdo->prepare('SELECT * FROM fr_users WHERE email = :value');
         } else {
             throw new Exception('Parameters must be id or handle');
         }
@@ -31,36 +40,25 @@ class UserModel {
         $stmt->bindParam(':value', $value);
         $stmt->execute();
 
+        // If the user doesn't exist return false
         if ( !$stmt->rowCount() ) {
             return false;
         }
         
         $row = $stmt->fetch();
 
-        if ($row['rank'] == 'admin') {
-            $user = new Admin();
-        } else {
-            $user = new User();
-        }
-
-        $user->setID($row['id']);
-        $user->setEmail($row['email']);
-        $user->setHandle($row['handle']);
-        $user->setPassword($row['password']);
-        $user->setDOB(strtotime($row['dob']));
-        $user->setSex($row['sex']);
-        $user->setLocation($row['location']);
-        $user->setTwitter($row['twitter']);
+        $user = $this->load($row);
 
         return $user;
     }
 
+    // Creates user objects for all users in the database and returns them as an array.
     function getAllUsers() {
-        $get = DB::getInstance()->prepare('SELECT * FROM fr_users');
+        $get = $this->pdo->prepare('SELECT * FROM fr_users');
         $get->execute();
-
+        
+        // Load a user object for each database entry
         $users = array();
-
         while ($row = $get->fetch()) {
             $users[] = $this->load($row);
         }
@@ -68,6 +66,7 @@ class UserModel {
         return $users;
     }
 
+    // Updates a user in the database.
     function save($user) {
         $email = $user->getEmail();
         $handle = $user->getHandle();
@@ -76,16 +75,17 @@ class UserModel {
         $location = $user->getLocation();
         $sex = $user->getSex();
         $twitter = $user->getTwitter();
+        $dob = date('Y-m-d', $user->getDOB());
 
-        if ($user->getDOB()) {
-            $dob = date('Y-m-d', $user->getDOB());
-        } else {
-            $dob = false;
-        }
-
-        $stmt = DB::getInstance()->prepare('UPDATE fr_users
-                                            SET email = :email, handle = :handle, password = :password, dob = :dob, sex = :sex, location = :location, twitter = :twitter
-                                            WHERE id = :user_id');
+        $stmt = $this->pdo->prepare('UPDATE fr_users
+                                     SET email = :email,
+                                        handle = :handle,
+                                        password = :password,
+                                        dob = :dob,
+                                        sex = :sex,
+                                        location = :location,
+                                        twitter = :twitter
+                                     WHERE id = :user_id');
         
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':handle', $handle);
@@ -95,19 +95,26 @@ class UserModel {
         $stmt->bindParam(':location', $location);
         $stmt->bindParam(':sex', $sex);
         $stmt->bindParam(':twitter', $twitter);
+
         $stmt->execute();
     }
 
-    function delete($user) {
+    // Deletes a user from the database.
+    public function delete($user) {
         $id = $user->getID();
-        $stmt = DB::getInstance()->prepare('DELETE FROM fr_users
-                                            WHERE id = :id');
+        $stmt = $this->pdo->prepare('DELETE FROM fr_users
+                                     WHERE id = :id');
         $stmt->bindParam(':id', $id);
         $stmt->execute();
     }
     
-    function load($row) {
-        $user = new User();
+    // Instantiates and returns  a user object using a row from the database.
+    private function load($row) {
+        if ($row['rank'] == 'admin') {
+            $user = new Admin();
+        } else {
+            $user = new User();
+        }
 
         $user->setID($row['id']);
         $user->setEmail($row['email']);
