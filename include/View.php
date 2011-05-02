cfbb3bdcdf178bafc14e0be187b0ad8caa7a4edc
kitano <danielfeely@gmail.com>
2011-05-02 14:29:38 +0100

    Added some documentation

--- include/View.php
+++ include/View.php
@@ -1,7 +1,14 @@
 <?php
 
+/**
+* View
+* Handles loading html pages.
+*/
+
 class View {
 
+    // Takes a filename/path to the view file and loads it.
+    // Optionally a data array can be passed to it if the view requires data from the calling controller.
     function load ($filename, $data = array()) {
         include ROOT_PATH . '/include/views/' . $filename . '.php';
     }
--- include/controllers/Controller.php
+++ include/controllers/Controller.php
@@ -1,14 +1,23 @@
 <?php
 
+/**
+* Controller
+* The Base Controller class.
+*/
+
 abstract class Controller {
 
+    // Each controller has a number of models loaded by default.
+    // The models provide access to the database and handle the business logic of the application.
     protected $userModel;
     protected $filmModel;
     protected $seenModel;
     protected $listModel;
 
+    // Each controller contains an instance of the View class that handles displaying the html pages.
     protected $view;
 
+    // The constructor simply instantiates the models and view.
     function __construct() {
         $this->userModel = new UserModel();
         $this->filmModel = new FilmModel();
@@ -17,15 +26,18 @@ abstract class Controller {
         $this->view = new View();
     }
 
+    // The index() function is required by all children of the Controller class.
+    // index() is called by default by index.php if no function has been specified.
     abstract function index();
 
+    //sends the user to the login page
     function redirectToLogIn() {
         header("Location: /login");
     }
 
+    // logs the user out and redirects to the home page
     function logOut() {
         LoginManager::getInstance()->logOut();
-
         header("Location: /");
     }
 }
