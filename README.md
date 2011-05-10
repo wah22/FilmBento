14feaae46f14ac82c4b855ec1d4e037846a419b0
kitano <danielfeely@gmail.com>
2011-05-10 19:43:17 +0100

    Added the ad banner in the footer

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # FilmBento
+It's a simple website that based around user's profiles which display what films they've seen recently and what they thought of them.
 
 ## Structure
 The application is based on a simple MVC framework.
+
--- include/views/footer.php
+++ include/views/footer.php
@@ -1,4 +1,4 @@
-<div style="clear:both">
+<div style="clear:both"></div>
 <footer>
     <ul>
         <li><a href="<?php echo BASE_URL; ?>/faq">FAQ</a></li>
@@ -6,6 +6,11 @@
         <li><a href="<?php echo BASE_URL; ?>/about">About</a></li>
     </ul>
     Copyright &copy; 2011 FilmBento. Licensed under <a href="http://www.gnu.org/licenses/gpl.html">GPLv3</a>.
+
+<div style="text-align:center; margin-top: 20px; margin-bottom: 20px;">
+<a href='http://openshare.emotionull.com/init/default/r/24ba8b97319ecf32b5f8bac8af92d05e7a0b3b11'><img style='border:1px solid #e3e3e3' src='http://openshare.emotionull.com/init/default/b/24ba8b97319ecf32b5f8bac8af92d05e7a0b3b11'/></a>
+</div>
+
 </footer>
 
 <!-- Start of StatCounter Code -->
@@ -22,3 +27,4 @@ href="http://statcounter.com/joomla/" target="_blank"><img
 class="statcounter" src="http://c.statcounter.com/6843054/0/d6b25fcb/1/"
 alt="hit counter joomla" ></a></div></noscript>
 <!-- End of StatCounter Code -->
+
